3c9a0fed2e002b8aa13b6c29729a1d3fbb1bec11
Shehab218 <sarkershehab123@gmail.com>
2025-04-14 20:58:15 +0600

    some change

--- Guess the number(computer)/main.py
+++ Guess the number(computer)/main.py
@@ -20,7 +20,7 @@ def computer_guess(x):
             guess=random.randint(low,high)
         else:
             guess=low
-        feedback = input(f"Is {guess} too high (H), too low (L) , or correct (C)??")
+        feedback = input(f"Is {guess} too high (H), too low (L) , or correct (C) ??:")
         if feedback =='h':
             high = guess - 1
         elif feedback == 'l':
